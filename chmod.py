c0b9dbde6883b92bfe02e2fa144409381957281e
Jan T. Sott <jan@idleberg.com>
2016-04-18 12:51:14 +0200

    use join() for script path

--- chmod.py
+++ chmod.py
@@ -1,13 +1,13 @@
 import os, stat, sublime, sublime_plugin
 
-# Configuration
-pkg = 'NSIS'
-script = sublime.packages_path() + '/' + pkg + '/nsis_build.sh'
-
 def plugin_loaded():
+    from os.path import join
     from package_control import events
 
+    package = "NSIS"
+    script  = join(sublime.packages_path(), package + "/nsis_build.sh")
+
     # chmod +x <script>
-    if (events.install(pkg) or events.post_upgrade(pkg)) and os.name is 'posix' or 'mac':
+    if (events.install(package) or events.post_upgrade(package)) and os.name is 'posix' or 'mac':
         st = os.stat(script)
         os.chmod(script, st.st_mode | stat.S_IEXEC)
